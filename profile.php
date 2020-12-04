b972738db883a4c43273d661299ecc8168becd4d
Param Joshi <50999496+padfoot33@users.noreply.github.com>
2020-12-05 01:22:58 +0530

    End Commit

---
+++ profile.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+?>
+//targeting session IDs
+<?php
+$id = $_SESSION['id'];
+$email = $_SESSION['userSession'];
+
+?>
+<p align="right"><a href="logout.php">Logout</a> </p>
+<h1 align="center">Welcome Mr.<?php echo $_SESSION['name']; ?></h1>
+    <p align="center">Your Email is : <?php echo $email ?></p>
